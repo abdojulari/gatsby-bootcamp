d2c31fd51db134541536f5b19ae78710157144ba
Abdul <abdul@iversoft.ca>
2021-03-09 10:03:48 -0500

    splash screen

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,9 +59,6 @@ module.exports = {
       },
       {
         resolve: `gatsby-plugin-offline`,
-        // options: {
-        //   precachePages: [`/`,`/about/`, `/blog/*`, `/contact/`],
-        // },
       },
   ]
 
